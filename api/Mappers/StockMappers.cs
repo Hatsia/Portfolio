35f198fddca81b1bfc139fc742b8391a8a0ab894
Ivan <ivan.serdyukov270@gmail.com>
2024-12-14 19:24:56 +0300

    Add DTOs and mappers

---
+++ api/Mappers/StockMappers.cs
@@ -0,0 +1,22 @@
+﻿using api.DTOs.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Indastry = stockModel.Indastry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+    }
+}
