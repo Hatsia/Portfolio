e88998746be72b60937dd6b991a4d3236780058e
Ivan <ivan.serdyukov270@gmail.com>
2024-12-14 20:37:02 +0300

    Create Update and Delete funcs, Add UpdateDTO

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -29,8 +29,8 @@ namespace api.Controllers
         {
             var stock = _context.Stocks.Find(id);
 
-            if (stock == null) 
-            { 
+            if (stock == null)
+            {
                 return NotFound();
             }
 
@@ -45,7 +45,45 @@ namespace api.Controllers
             _context.Stocks.Add(stockModel);
             _context.SaveChanges();
 
-            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id}, stockModel.ToStockDto());
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Indastry = updateDto.Indastry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if(stockModel == null)
+                return NotFound();
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
