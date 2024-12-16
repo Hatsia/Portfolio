e5f28282dd3fbfd17785eeb188c7ce632444d455
Ivan <ivan.serdyukov270@gmail.com>
2024-12-16 22:11:11 +0300

    Refactor to repository pattern

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -1,9 +1,18 @@
-﻿using api.Models;
+﻿using api.DTOs.Stock;
+using api.Models;
 
 namespace api.Interfaces
 {
     public interface IStockRepository
     {
         Task<List<Stock>> GetAllAsync();
+
+        Task<Stock?> GetByIdAsync(int id);
+
+        Task<Stock> CreateAsync(Stock stockModel);
+
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockModel);
+
+        Task<Stock?> DeleteAsync(int id);
     }
 }
--- api/Program.cs
+++ api/Program.cs
@@ -31,4 +31,3 @@ app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
-
