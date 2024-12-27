54f4671f36f75f00110395cdabc3ebec7041fda3
Ivan <ivan.serdyukov270@gmail.com>
2024-12-27 07:00:53 +0300

    Create Register account, update dto-s, add migration

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -1,19 +1,43 @@
 ﻿using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<AppUser >
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
         {
-            
+
         }
 
         public DbSet<Stock> Stocks { get; set; }
-        
+
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = "1",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                },
+                new IdentityRole
+                {
+                    Id = "2",
+                    Name = "User",
+                    NormalizedName = "USER",
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
 
     }
 }
